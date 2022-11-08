7ccea3bfe6beae76579cff79cdb7a88854663ee0
Danyil Tuhaienko <kpz21_tdo@student.ztu.edu.ua>
2022-11-08 11:35:00 +0200

    Outputs changed

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -1,8 +1,8 @@
 ﻿using System.Text;
 
 Console.OutputEncoding = Encoding.UTF8;
-Console.WriteLine("Курси Sana Commerce Software Development Bootcamp");
-Console.WriteLine("Курси Sana Commerce Software Development Bootcamp");
-Console.WriteLine("Курси Sana Commerce Software Development Bootcamp");
+Console.WriteLine("Output 1");
+Console.WriteLine("Output 2");
+Console.WriteLine("Output 3");
 Console.WriteLine("Курси Sana Commerce Software Development Bootcamp");
 Console.WriteLine("Курси Sana Commerce Software Development Bootcamp");
