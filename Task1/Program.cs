1fa3f2ae1e1fb72775524ec569979685f7e28d27
Danyil Tuhaienko <kpz21_tdo@student.ztu.edu.ua>
2022-11-08 11:30:22 +0200

    Output duplicated 5 times

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -2,3 +2,7 @@
 
 Console.OutputEncoding = Encoding.UTF8;
 Console.WriteLine("Курси Sana Commerce Software Development Bootcamp");
+Console.WriteLine("Курси Sana Commerce Software Development Bootcamp");
+Console.WriteLine("Курси Sana Commerce Software Development Bootcamp");
+Console.WriteLine("Курси Sana Commerce Software Development Bootcamp");
+Console.WriteLine("Курси Sana Commerce Software Development Bootcamp");
